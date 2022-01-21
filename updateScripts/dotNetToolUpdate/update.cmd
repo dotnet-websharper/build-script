65f1d4226f29ad50d84cdc5d51bbc68e30b9b079
András Jankó <andras.janko@gmail.com>
2022-01-21 18:57:01 +0100

    fix to updateScripts/dotNetToolUpdate

--- updateScripts/dotNetToolUpdate/update.cmd
+++ updateScripts/dotNetToolUpdate/update.cmd
@@ -1,6 +1,8 @@
 dotnet tool update paket
 dotnet tool update fake-cli
 
-git add -A
-git commit -m "Update dotnet tools"
-:: git push
+git add -A 
+git commit -m "Update dotnet tools" 
+if errorlevel 1 exit 0
+
+git push
