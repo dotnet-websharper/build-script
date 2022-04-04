17b6bef47c3064345f95f9c0b6cabf3d27d4ade3
András Jankó <andras.janko@gmail.com>
2022-04-04 21:02:59 +0200

    test-templates: use different ports for testing

--- test-templates.fsx
+++ test-templates.fsx
@@ -3,7 +3,7 @@ open System.Diagnostics
 open System.Threading
 open System.Net.Http
 
-let runSite relPath =
+let runSite relPath port =
     let mutable startedOk = false
     let started = new EventWaitHandle(false, EventResetMode.ManualReset)    
     
@@ -11,7 +11,7 @@ let runSite relPath =
 
     use proc = new Process()
     proc.StartInfo.FileName <- "dotnet"
-    proc.StartInfo.Arguments <- "run --project " + fullPath
+    proc.StartInfo.Arguments <- $"run --project {fullPath} --urls=http://localhost:{port}/"
     proc.StartInfo.UseShellExecute <- false
     proc.StartInfo.RedirectStandardOutput <- true
     
@@ -24,7 +24,7 @@ let runSite relPath =
     )
     proc.Exited.Add(fun _ -> 
         if not startedOk then
-            failwithf "Starting project %s failed." relPath
+            failwith $"Starting project {relPath} failed."
     )
 
     proc.Start() |> ignore
@@ -37,20 +37,19 @@ let runSite relPath =
         fun _ _ _ _ -> true
     use client = new HttpClient(handler, true)
 
-    let resp = client.GetAsync("http://localhost:5000/").Result
+    let resp = client.GetAsync($"http://localhost:{port}/").Result
 
     if not resp.IsSuccessStatusCode then
-        failwithf "Getting home page of project %s failed." relPath    
+        failwith $"Getting home page of project {relPath} failed."     
     else
-        printfn "Testing %s ok." relPath
+        printfn $"Testing {relPath} ok." 
 
     proc.Kill()
-    Thread.Sleep(3000)
     printfn "Run stopped."
     
-runSite "Fs.Web/Fs.Web.fsproj"
-runSite "Cs.Web/Cs.Web.csproj"
-runSite "Fs.Spa/Fs.Spa.fsproj"
-runSite "Cs.Spa/Cs.Spa.csproj"
-runSite "Fs.Min/Fs.Min.fsproj"
-runSite "Cs.Min/Cs.Min.csproj"
+runSite "Fs.Web/Fs.Web.fsproj" 5000
+runSite "Cs.Web/Cs.Web.csproj" 5010
+runSite "Fs.Spa/Fs.Spa.fsproj" 5020
+runSite "Cs.Spa/Cs.Spa.csproj" 5030
+runSite "Fs.Min/Fs.Min.fsproj" 5040
+runSite "Cs.Min/Cs.Min.csproj" 5050
