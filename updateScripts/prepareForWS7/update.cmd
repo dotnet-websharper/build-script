0fdfed58c215ed59c23cf0474fbc04df62e1ba0b
András Jankó <andras.janko@gmail.com>
2022-12-28 11:13:55 +0100

    fix prepareForWS7

--- updateScripts/prepareForWS7/update.cmd
+++ updateScripts/prepareForWS7/update.cmd
@@ -1,12 +1,14 @@
 @ECHO OFF
 
+git branch -b websharper70
 git checkout websharper70
 git push -u origin websharper70
-git checkout master
-dotnet fsi fix60Dependencies.fsx
 
+git checkout master
+dotnet fsi %~dp0\fix60Dependencies.fsx
 git add -A 
 git commit -m "Fix WebSharper dependencies to 6.x"
-if errorlevel 1 exit 0
 
 git push
+
+exit 0
