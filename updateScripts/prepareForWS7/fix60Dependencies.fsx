06ade928d477c4e69de3a1a7b42935715be3ddc6
András Jankó <andras.janko@gmail.com>
2022-12-28 09:57:47 +0100

    Update prepareForWS7

--- updateScripts/prepareForWS7/fix60Dependencies.fsx
+++ updateScripts/prepareForWS7/fix60Dependencies.fsx
@@ -1,5 +1,5 @@
 open System.IO
 
 File.ReadAllLines "paket.dependencies"
-|> Array.map (fun s -> if s.Contains("WebSharper") then s.Replace("prerelease", "") else s)
+|> Array.map (fun s -> if s.Contains("WebSharper") then s.Replace("prerelease", "~> 6.0") else s)
 |> fun ls -> File.WriteAllLines("paket.dependencies", ls)
--- updateScripts/prepareForWS7/update.cmd
+++ updateScripts/prepareForWS7/update.cmd
@@ -1,19 +1,12 @@
 @ECHO OFF
-for /f %%i in ('git.exe rev-parse --show-toplevel') do (
-    SET reponame=%%~ni
-)
 
-if "%reponame%" NEQ "core" (
-    git branch -b websharper70
-    git push -u origin websharper70
-    git checkout master
-    dotnet fsi removePrerelease.fsx
-) else (
-    dotnet fsi removePrerelease.fsx
-)
+git checkout websharper70
+git push -u origin websharper70
+git checkout master
+dotnet fsi fix60Dependencies.fsx
 
 git add -A 
-git commit -m "Use non-prerelease packages for WS6"
+git commit -m "Fix WebSharper dependencies to 6.x"
 if errorlevel 1 exit 0
 
 git push
