1ac8bc6043dee8e1e9899fc25c1e95baceea5eab
Jooseppi12 <urijozsef@europe.com>
2023-11-22 14:18:06 +0100

    Update update.ps1

--- updateScripts/websharper70tomaster/update.ps1
+++ updateScripts/websharper70tomaster/update.ps1
@@ -1,13 +1,13 @@
 $dirname = Split-Path -Path (Get-Location) -Leaf
 
-git branch websharper60
-git checkout websharper60
-git push -u origin websharper60
-
 if (($dirname -eq "core") -or ($dirname -eq "ui")) 
 {
 	exit 0
 }
+
+git branch websharper60
+git checkout websharper60
+git push -u origin websharper60
 
 git checkout websharper70
 gh pr create -t "WebSharper 7.0" -b "WebSharper 7.0"
