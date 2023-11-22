3d3763e6029319dc22defd0cd5795acc78832242
Jooseppi12 <urijozsef@europe.com>
2023-11-22 17:32:54 +0100

    Update update.ps1 - Relog with proper token

--- updateScripts/websharper70tomaster/update.ps1
+++ updateScripts/websharper70tomaster/update.ps1
@@ -9,5 +9,8 @@ git branch websharper60
 git checkout websharper60
 git push -u origin websharper60
 
+echo "${{ secrets.PAT_PR }}" > token.txt
+gh auth login --with-token < token.txt
+
 git checkout websharper70
 gh pr create -t "WebSharper 7.0" -b "WebSharper 7.0"
