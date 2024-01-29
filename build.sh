07833f6794b2556ade4f1c5d8f621ad7a8d1e20a
Jooseppi12 <urijozsef@europe.com>
2024-01-29 16:39:39 +0100

    Remove explicit msbuildversion

--- build.sh
+++ build.sh
@@ -7,9 +7,9 @@ if [ "$BuildBranch" != "" ]; then
     export BuildFromRef=$(<build/buildFromRef)
 fi
 
-if [ "$VisualStudioVersion" == ""  ]; then
-    export VisualStudioVersion=15.0
-fi
+# if [ "$VisualStudioVersion" == ""  ]; then
+#     export VisualStudioVersion=15.0
+# fi
 
 # Allow running `build SomeTask` instead of `build -t SomeTask`
 if [ "$1" != "" -a "$1" != -t -a "$1" != --target ]; then EXTRA_ARG=-t; else EXTRA_ARG=; fi
