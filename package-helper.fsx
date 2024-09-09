1f3ec9d7f43fae11a1000b67f9e3071c8b302839
Jooseppi12 <urijozsef@europe.com>
2024-09-09 12:25:53 +0200

    Testing job summary

---
+++ package-helper.fsx
@@ -0,0 +1,12 @@
+System.IO.Directory.EnumerateFiles("localnuget")
+|> Seq.map (fun f ->
+    let file = System.IO.FileInfo(f)
+    let filename = file.Name.Replace(".nupkg", "")
+    let splitByDots = filename.Split([|'.'|])
+    let len = splitByDots.Length
+    let name = splitByDots[0..len-5] |> String.concat "."
+    let version = splitByDots[len-4..] |> String.concat "."
+    sprintf "| %s | %s |" name version
+)
+|> String.concat "\n"
+|> printfn "%s"
