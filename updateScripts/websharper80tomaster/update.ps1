df50170668f951a70f2b0955fcf37c40ba048744
András Jankó <andras.janko@gmail.com>
2025-02-04 16:54:37 +0100

    updateScripts/websharper80tomaster: test withouth gh auth

--- updateScripts/websharper80tomaster/update.ps1
+++ updateScripts/websharper80tomaster/update.ps1
@@ -9,7 +9,5 @@ git branch websharper70
 git checkout websharper70
 git push -u origin websharper70
 
-gh auth login --with-token ${env:PR_TOKEN}
-
 git checkout net8upgrade
 gh pr create -t "WebSharper 8.0" -b "WebSharper 8.0"
